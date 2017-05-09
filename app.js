cece9cf677169b5b19e3a50d571bc883f291523d
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-09 17:49:17 -0600

    App.js setup

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
+var express          = require('express');
+var path             = require('path');
+var cookieParser     = require('cookie-parser');
+var bodyParser       = require('body-parser');
+var exphbs           = require('express-handlebars');
 var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var flash            = require('connect-flash');
+var session          = require('express-session');
+var passport         = require('passport');
+var LocalStrategy    = require('passport-local').Strategy;
+var mongo            = require('mongodb');
+var mongoose         = require('mongoose');
 
 mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
+
+var routes = erquire('./routes/index');
+var users  = require('./routes/users');
+
+// Initialize app
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// Set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
